6bc56b625e5f93c26744a62d3d80db465c2e9565
barotovbekhruzbek <barotovbekhruzbek@gmail.com>
2023-08-27 17:46:25 +0500

    header

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        <div class="logo">Logo</div>
+        <el-button type="danger">
+          <el-icon>
+            <SwitchButton />
+          </el-icon>
+        </el-button>
+    </div>
+</template>
+
+<script>
+import {SwitchButton} from "@element-plus/icons-vue";
+
+export default {
+  components: {SwitchButton}
+
+}
+</script>
+
+<style lang="scss">
+  @import "@/styles/components/header.scss";
+</style>
--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -11,6 +11,6 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import url("../styles/main.scss");
 </style>
